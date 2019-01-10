bc357c281844fd01372f2aeee2157705129c8118
Fatai Balogun <fattylee.remod@gmail.com>
2019-01-10 16:07:33 +0100

    complete post and put api's

--- app-express.js
+++ app-express.js
@@ -1,17 +1,19 @@
 const express = require ("express");
 const app = express();
+// const bodyParser = require('body-parser');
 
+app. use(express. json());
 const courses = [
 {id: 1, name: 'course1'},
 {id: 2, name: 'course2'}, 
 ];
 
-app. get('/api/customers', (req, res)=>{
+app. get('/api/courses', (req, res)=>{
 // res. send(["abu", "ummu", 'Abdullah', {age:31 }]);
 res. status(200). send(courses);
 });
 
-app. get('/api/customers/:id/', (req, res)=>{
+app. get('/api/courses/:id/', (req, res)=>{
 
 const {id} = req. params ;
 
@@ -25,14 +27,41 @@ if (!course) return res. status(404). send("The course with the given ID:"+ req.
 " was not found!"  );
 
 return res. status (200). send(course);
-/*
-const objRes = {
-["query"] : req. query,
-params: req. params, 
-}
-res. send( objRes  )
-*/
-})
+
+});
+
+app. post('/api/courses', (req, res)=>{
+const { name } = req. body;
+
+if(!name) return res.status(400). send ({message:'name field can\'t be "undefined" or "empty"'});
+const id = courses[courses. length - 1]. id + 1;
+const course = {id, name};
+courses. push(course);
+
+return res.status(201). send(course);
+});
+
+app. put('/api/courses/:id', (req, res)=>{
+const { id } = req. params ;
+const { name } = req. body ;
+
+let message = 'Pls enter a valid course number';
+if(Number. isNaN(Number(id))) return res. status (403). send({message});
+
+message = 'name field can\'t be "undefined" or "empty"';
+if(!name) return res.status(400). send ({message});
+
+const course  = courses. find ((course_el)=> course_el. id == id);
+
+message = `the course with id: ${id} was not found`;
+if (!course) return res. status(404). send({message});
+
+const newCourse = {...course, name };
+const index = courses. indexOf(course);
+courses[index] = newCourse;
+
+return res.status(201). send(newCourse);
+});
 
 const port = process. env. PORT || 3000;
 
